cc21ce052e95e5fa83738bf854bf566abb6e9163
meta42 <648398613@qq.com>
2017-02-09 21:17:22 +0800

    fog

---
+++ GradientFog.cginc
@@ -0,0 +1,36 @@
+﻿#ifndef GRADIENT_FOG_INCLUDED
+#define GRADIENT_FOG_INCLUDED 
+
+#define GRADIENT_GLOBAL_FOG_COORDS(idx)			float2 fogCoord : TEXCOORD##idx;
+#define GRADIENT_GLOBAL_FOG_TRANSFER(o, posW)	o.fogCoord.xy = CalculateFogCoords(posW);
+#define GRADIENT_GLOBAL_FOG_APPLY(c, coord)		c.rgb = ApplyFog( c.rgb, coord.xy);
+
+uniform half depthFogDensity;
+uniform half depthFogGradientDelta;
+uniform half depthFogGradientStartAdd;
+uniform sampler2D depthFogGradientTexture;
+
+uniform half verticalFogDensity;
+uniform half verticalFogBaseHight;
+uniform sampler2D verticalFogGradientTexture;
+
+half2 CalculateGlobalFogCoords(float3 posWs)
+{
+	half2 fogCoord = 0.0;
+	half scale = distance(posWs, _WorldSpaceCameraPos);
+	fogCoord.x = (depthFogGradientDelta * scale  + depthFogGradientStartAdd) * depthFogDensity;
+	fogCoord.y = exp(-posWs.y - verticalFogBaseHight) * verticalFogDensity;
+	return saturate(fogCoord.xy);
+}
+
+half3 ApplyGlobalFog(half3 c, half2 fogCoord)
+{
+	half4 depthFogColor = tex2D(depthFogGradientTexture, half2( fogCoord.x, 0.0f)).rgba;
+	c.rgb = lerp(c.rgb, depthFogColor.rgb, depthFogColor.a);
+
+	half4 verticalFogColor = tex2D(verticalFogGradientTexture, half2( fogCoord.y, 0.0f)).rgba;
+	c.rgb = lerp(c.rgb, verticalFogColor.rgb,  fogCoord.y);
+	return c.rgb;
+}
+
+#endif
---
+++ GradientGlobalFog.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GradientGlobalFog : MonoBehaviour
+{
+    public int gradientTextureWidth = 32;
+
+    [Header("DepthFog")]
+
+    public float depthFogDensity;
+    public float depthFogStartDistance = 0.0f;
+    public float depthFogEndDistance = 100.0f;
+    public Gradient depthFogGradient = new Gradient();
+    Texture2D depthFogGradientTexture;
+
+    [Header("Vertical Fog")]
+    public float verticalFogDensity;
+    public float verticalFogBaseHight;
+    public Gradient verticalFogGradient = new Gradient();
+    Texture2D verticalFogGradientTexture;
+
+
+    void Start ()
+    {
+        UpdateConstants();
+    }
+
+    void Update()
+    {
+        //for Edit
+        if (Application.isEditor && !Application.isPlaying) UpdateConstants();
+    }
+
+    private void UpdateConstants()
+    {
+        GenerateTexture();
+
+        float scope = depthFogEndDistance - depthFogStartDistance;
+        float delta = 1.0f / scope;
+        //calculate start distance add
+        float startAdd = -depthFogStartDistance / scope;
+
+        Shader.SetGlobalFloat("depthFogGradientDelta", delta);
+        Shader.SetGlobalFloat("depthFogGradientStartAdd", startAdd);
+        Shader.SetGlobalFloat("depthFogDensity", depthFogDensity);
+        Shader.SetGlobalFloat("verticalFogDensity", verticalFogDensity);
+        Shader.SetGlobalFloat("verticalFogBaseHight", verticalFogBaseHight);
+    }
+
+    private void GenerateTexture()
+    {
+        depthFogGradientTexture = new Texture2D(gradientTextureWidth, 1, TextureFormat.ARGB32, false);
+        depthFogGradientTexture.wrapMode = TextureWrapMode.Clamp;
+
+        float delta = 1.0f / (gradientTextureWidth - 1);
+        float value = 0.0f;
+        for (int i = 0; i < gradientTextureWidth; i++)
+        {
+            depthFogGradientTexture.SetPixel(i, 0, depthFogGradient.Evaluate(value));
+            value += delta;
+        }
+        depthFogGradientTexture.Apply();
+
+        Shader.SetGlobalTexture("depthFogGradientTexture", depthFogGradientTexture);
+
+        verticalFogGradientTexture = new Texture2D(gradientTextureWidth, 1, TextureFormat.RGB24, false);
+        verticalFogGradientTexture.wrapMode = TextureWrapMode.Clamp;
+
+        delta = 1.0f / (gradientTextureWidth - 1);
+        value = 0.0f;
+        for (int i = 0; i < gradientTextureWidth; i++)
+        {
+            verticalFogGradientTexture.SetPixel(i, 0, verticalFogGradient.Evaluate(value));
+            value += delta;
+        }
+        verticalFogGradientTexture.Apply();
+
+        Shader.SetGlobalTexture("verticalFogGradientTexture", verticalFogGradientTexture);
+
+    }
+
+}
